432fb8873d69ded0757c5a8f8fd8ac22da65de78
achernomyrdin <achernomyrdin@IMAC-202-201-PC.itf.local>
2022-05-31 11:19:19 +0500

    itfi-mgn-new

--- src/lesson7/IOTest.java
+++ src/lesson7/IOTest.java
@@ -12,6 +12,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.Reader;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class IOTest {
 
@@ -23,31 +28,58 @@ public class IOTest {
 		
 		
 		try(final Duration	d = new Duration();
-			final OutputStream os = new ByteArrayOutputStream();//new FileOutputStream("c:/temp/test.txt");
-			final PrintStream ps = new PrintStream(os)) {
+			final OutputStream os = /*new ByteArrayOutputStream();*/new FileOutputStream("c:/temp/test.zip");
+//			final GZIPOutputStream gzos = new GZIPOutputStream(os);
+			final ZipOutputStream zos = new ZipOutputStream(os)) {
+			
+			ZipEntry	ze = new ZipEntry("a/Part 1");
+			ze.setMethod(ZipEntry.DEFLATED);
+			zos.putNextEntry(ze);
+			
+			PrintStream ps = new PrintStream(zos);
 			
 			ps.println("test string"/*.getBytes()*/);
 			ps.flush();
-			temp = ((ByteArrayOutputStream)os).toByteArray();
+			
+//			zos.closeEntry();
+			ze = new ZipEntry("a/Part 2");
+			ze.setMethod(ZipEntry.DEFLATED);
+			zos.putNextEntry(ze);
+			
+			ps = new PrintStream(zos);
+			
+			ps.println("test string 2"/*.getBytes()*/);
+			ps.flush();
+	
+//			zos.finish();
+//			temp = ((ByteArrayOutputStream)os).toByteArray();
 		} // finally {os.close();}
 		// java.lang.AutoCloseable
 		//
 		byte[]	buffer = new byte[100];
 		char[]	bufferC = new char[100];
 		
-		try(final InputStream is = new ByteArrayInputStream(temp);//new FileInputStream("c:/temp/test.txt");
-			final Reader rdr = new InputStreamReader(is);
-			final BufferedReader brdr = new BufferedReader(rdr)) {
-			int	len;
-			
-//			while ((len = rdr.read(bufferC)) > 0) {
-//				System.err.println("Len: "+len);
-//				System.err.println("Data="+new String(buffer,0,len));
-//			}
-			String line;
-			
-			while ((line = brdr.readLine()) != null) {
-				System.err.println("Data="+line);
+		try(final InputStream is = /*new ByteArrayInputStream(temp);*/new FileInputStream("c:/temp/test.zip");
+//			final GZIPInputStream gzis = new GZIPInputStream(is);
+			ZipInputStream	zis = new ZipInputStream(is)) {
+				
+			ZipEntry ze;
+			while ((ze = zis.getNextEntry()) != null) {
+				System.err.println("Name="+ze.getName());	
+					
+				final Reader rdr = new InputStreamReader(zis);
+				final BufferedReader brdr = new BufferedReader(rdr);
+				int	len;
+				
+	//			while ((len = rdr.read(bufferC)) > 0) {
+	//				System.err.println("Len: "+len);
+	//				System.err.println("Data="+new String(buffer,0,len));
+	//			}
+				String line;
+				
+				while ((line = brdr.readLine()) != null) {
+					System.err.println("Data="+line);
+				}
 			}
 		}
 	}
